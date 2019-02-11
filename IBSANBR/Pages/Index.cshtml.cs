8e23b5882d03c7ac9a8283d7f1ec93d966bb2fe1
Bruno <Bruno@PLATINUM>
2019-02-11 15:41:30 -0300

    Primeira publicação...

--- IBSANBR/Pages/Index.cshtml.cs
+++ IBSANBR/Pages/Index.cshtml.cs
@@ -18,12 +18,28 @@ namespace IBSANBR.Pages
             _municipioRepository = municipioRepository;
         }
 
+        [FromForm]
         public string[] MunicipiosSelecionados { get; set; }
         public List<Municipio> Municipios { get; set; }
+
+        public List<Municipio> MunicipiosInfo { get; set; } = new List<Municipio>();
 
         public async Task<IActionResult> OnGetAsync()
         {
             Municipios = await _municipioRepository.Listar();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            Municipios = await _municipioRepository.Listar();
+            if(MunicipiosSelecionados.Length > 0)
+            {
+                foreach(var m in MunicipiosSelecionados)
+                {
+                    MunicipiosInfo.Add(Municipios.Where(x=>x.Codigo == m).SingleOrDefault());
+                }                
+            }
             return Page();
         }
     }
