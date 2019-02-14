a73e89e2d0cca7775e6bc3b8504dc78077293a8f
Bruno Tourinho <brunotop@gmail.com>
2019-02-13 21:38:16 -0300

    Revert "Alterações"
    This reverts commit efef0a1c0144799cc148386eb525faac3a595f85, reversing
    changes made to ad28f3e94f47174e545968470bc8d653396faf8c.

--- IBSANBR/Models/PopulacaoCobertura.cs
+++ IBSANBR/Models/PopulacaoCobertura.cs
@@ -8,9 +8,11 @@ namespace IBSANBR.Models
     public class PopulacaoCobertura
     {
         public string Competencia { get; set; }
+        public string Prestador { get; set; }
         public decimal GE12A { get; set; }
+        public decimal GE06A { get; set; }
         public decimal AG001 { get; set; }
-        public decimal ES001 { get; set; }
+        public decimal ES026 { get; set; }
     }
 }
 
