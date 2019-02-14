3360cbbbd3f005410cac860f41b46aaa4a525ab7
Bruno Tourinho <brunotop@gmail.com>
2019-02-13 21:38:27 -0300

    Revert "Graficos adicionados ao menu"
    This reverts commit ad28f3e94f47174e545968470bc8d653396faf8c.

--- IBSANBR/Pages/Index.cshtml.cs
+++ IBSANBR/Pages/Index.cshtml.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IBSANBR.Models;
+using IBSANBR.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace IBSANBR.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly InformacoesRepository _infoRepository;
+
+        public IndexModel(InformacoesRepository infoRepository)
         {
+            _infoRepository = infoRepository;
+        }
+
+        [FromForm]
+        public string CodigoMunicipio { get; set; }
+
+        public List<Municipio> Municipios { get; set; }
+
+        public List<PopulacaoCobertura> PopulacaoCobertura { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Municipios = await _infoRepository.Listar();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            Municipios = await _infoRepository.Listar();
+            PopulacaoCobertura = await _infoRepository.PopulacaoCobertura(CodigoMunicipio);
+            return Page();
         }
     }
 }
