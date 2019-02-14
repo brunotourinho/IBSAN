2e292f165a8b701c361a343e67f38c6d3fe19071
Bruno <Bruno@PLATINUM>
2019-02-14 08:31:43 -0300

    Sincronização dos Códigos!

--- IBSANBR/Pages/Index.cshtml.cs
+++ IBSANBR/Pages/Index.cshtml.cs
@@ -1,41 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using IBSANBR.Models;
-using IBSANBR.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace IBSANBR.Pages
+namespace IBSANBR
 {
     public class IndexModel : PageModel
     {
-        private readonly InformacoesRepository _infoRepository;
-
-        public IndexModel(InformacoesRepository infoRepository)
+        public void OnGet()
         {
-            _infoRepository = infoRepository;
-        }
-
-        [FromForm]
-        public string CodigoMunicipio { get; set; }
-
-        public List<Municipio> Municipios { get; set; }
-
-        public List<PopulacaoCobertura> PopulacaoCobertura { get; set; }
-
-        public async Task<IActionResult> OnGetAsync()
-        {
-            Municipios = await _infoRepository.Listar();
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            Municipios = await _infoRepository.Listar();
-            PopulacaoCobertura = await _infoRepository.PopulacaoCobertura(CodigoMunicipio);
-            return Page();
         }
     }
 }
